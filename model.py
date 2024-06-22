e830f54f41d50d5e039a5a9318e041688067d346
jhk63 <163096026+jhk63@users.noreply.github.com>
2024-06-23 00:12:05 +0900

    update

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Basic CNN Model
+# CNN Model
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
